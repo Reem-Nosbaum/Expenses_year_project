818282cbe280e85dc8a07096f320686663a3ea6d
reemnu <reemnu@gmail.com>
2022-10-31 09:10:20 +0200

    Lifting The State Up

--- src/App.js
+++ src/App.js
@@ -23,9 +23,15 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+const addExpenseHandeler = expense => {
+console.log ('In app.js');
+console.log(expense)
+};
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandeler} />
       <Expenses items={expenses} />
     </div>
   );
